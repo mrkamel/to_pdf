5867c2126e612aee01a4a5b3447fcb68f00e9c6b
Benjamin Vetter <vetter@plainpicture.de>
2013-05-12 11:50:43 +0200

    Compatability with ruby 1.9

--- lib/to_pdf.rb
+++ lib/to_pdf.rb
@@ -1,12 +1,20 @@
 
 require "to_pdf/version"
 
+if RUBY_VERSION < "1.9"
+  require "iconv"
+end
+
 class String
   def to_pdf
     data = nil
 
     IO.popen("/usr/bin/htmldoc -t pdf --no-toc --quiet --footer ... --header ... --no-title --webpage --bodyfont helvetica -", "w+") do |pipe| 
-      doc = Iconv.conv("iso-8859-1", "utf-8", self) rescue self 
+      if RUBY_VERSION < "1.9"
+        doc = Iconv.conv("iso-8859-1", "utf-8", self)
+      else
+        doc = encode(Encoding::ISO_8859_1)
+      end
 
       pipe.write doc 
 
