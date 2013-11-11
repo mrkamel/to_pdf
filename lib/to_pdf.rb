b4ec84a90f9740d2148002040e6f47649e16f525
Benjamin Vetter <vetter@plainpicture.de>
2013-11-11 18:18:09 +0100

    ruby 1.8 support dropped, encoding issues fixed

--- lib/to_pdf.rb
+++ lib/to_pdf.rb
@@ -1,27 +1,17 @@
 
 require "to_pdf/version"
 
-if RUBY_VERSION < "1.9"
-  require "iconv"
-end
-
 class String
   def to_pdf
     data = nil
 
-    IO.popen("/usr/bin/htmldoc -t pdf --no-toc --quiet --footer ... --header ... --no-title --webpage --bodyfont helvetica -", "w+") do |pipe| 
-      if RUBY_VERSION < "1.9"
-        doc = Iconv.conv("iso-8859-1", "utf-8", self)
-      else
-        doc = encode(Encoding::ISO_8859_1)
-      end
-
-      pipe.write doc 
+    IO.popen("/usr/bin/htmldoc -t pdf --no-toc --quiet --footer ... --header ... --no-title --webpage --bodyfont helvetica -", "w+") do |pipe|
+      pipe.write encode(Encoding::ISO_8859_1, :undef => :replace, :invalid => :replace, :replace => "")
 
       pipe.close_write
 
       data = pipe.read
-    end 
+    end
 
     data
   end
